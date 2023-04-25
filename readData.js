f57c7502dab226736a247ce26348e42b8feace49
Nicolasfmc <nicolasfmc1@gmail.com>
2023-04-25 11:30:20 -0300

    feature using counting sort to sort the json array

--- readData.js
+++ readData.js
@@ -1,13 +1,17 @@
 const fs = require('fs');
+const { performance } = require('perf_hooks');
 const { parser } = require('stream-json');
 const { streamArray } = require('stream-json/streamers/StreamArray');
+const { stdin, stdout } = require('node:process');
+const readline = require('readline');
 var inputData = [];
+
+const rl = readline.createInterface({ input: stdin, output: stdout });
 
 // Função para processar cada objeto JSON lido do arquivo, colocando-o em um vetor
 async function processJsonObject(jsonObj) {
 //   console.log(jsonObj);
   inputData.push(jsonObj);
-
 }
 
 // Caminho do arquivo JSON de origem
@@ -34,22 +38,56 @@ jsonStream.on('error', (error) => {
   console.error(`Erro ao analisar o JSON: ${error.message}`);
 });
 
-function mostraVetor(){
-  inputData.forEach(e => {
-        console.log(e);
-        console.log('*********');
-  });
-}
+// desenvolvimento de ordernação
 
-function orderData(json) {
-    const newJson = json.sort((a, b) => a.log - b.log);
-    return console.log(newJson[999999]);
+// perguntar ao usuario o mes e o index do impostor
+// order p/mes dps order p/log e mostrar o index que foi enviado pelo usuario
+async function orderData(json) {
+  // for i = 1 to k do
+  // C[i] = C[i]+C[i−1]; {C[i] é número de j’s tais que A[j] ≤ i}
+  // end for
+  // for i = n decrescendo até 1 do
+  // B[C[A[j]]] = A[j];
+  // C[A[j]] = C[A[j]] − 1;
+  // end for
+  const arr = [];
+
+  for (let i = 0; i < 1000000; i++) {
+    arr[i].push(0);
+
+    return;
+  }
+
+  for (let j = 0; j < json.length; j++) {
+    arr[json[i]] = arr[json[i]] + 1;
+
+    return;
+  }
+
+  for (let idx = 0; idx < 1000000; idx++) {
+    arr[idx] = arr[idx] + arr[idx - 1];
+     return;
+  }
+
+  for (let index = 0; json.length < 1; index--) {
+    const b = [];
+    b[arr[json[index]]] = json[index];
+    arr[json[index]] = arr[json[index]] - 1;
+    
+    return b;
+  }
 }
 
 // Finaliza o processo ao terminar a leitura
 jsonStream.on('end', () => {
   console.log('Leitura do arquivo concluída.');
+  const start = performance.now();
+  
   //daqui deve partir o desenvolvimento, pois antes não garante que o arquivo está lido
-  console.log(inputData[0]);
   orderData(inputData);
+  
+  const end = performance.now()
+  
+  console.log('tempo de execucao:', ((end - start).toFixed(3)).substring(2) + 'ms');
+  return;
 });
